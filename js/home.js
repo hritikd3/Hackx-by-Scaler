41c109f397f48b04571f8957850ee2deb3b19279
hritik <86347286+hritikd3@users.noreply.github.com>
2021-10-10 12:20:47 +0530

    Added files for NTF

---
+++ js/home.js
@@ -0,0 +1,97 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  searchForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+
+let filterBtn = document.querySelectorAll('.filter-buttons .buttons');
+let filterItem = document.querySelectorAll('.products .box-container .box');
+
+filterBtn.forEach(button =>{
+
+  button.onclick = () =>{
+    filterBtn.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    let dataFilter = button.getAttribute('data-filter');
+
+    filterItem.forEach(item =>{
+
+      item.classList.remove('active');
+      item.classList.add('hide');
+
+      if(item.getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+        item.classList.remove('hide');
+        item.classList.add('active');
+      }
+
+    });
+
+  };
+
+});
+
+
+
+
+var swiper = new Swiper(".books-slider", {
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".featured-slider", {
+  centeredSlides: true,
+  loop:true,
+  spaceBetween:20,
+  autoplay: {
+    delay: 2200,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
